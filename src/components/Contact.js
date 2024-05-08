ade87756e928db2dd68d077a84e8bf999254da99
unknown <ezekielugweje3@gmail.com>
2024-05-07 17:16:31 -0700

    Updated code to my portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,6 @@ const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-
- 
-
   return (
     <section className='py-16 lg:section' id='contact'>
       <div className='container mx-auto'>
@@ -22,12 +19,13 @@ const Contact = () => {
               </h2>
             </div>
           </div>
-          <form  className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+          <form  className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start' method='post' action='https://getform.io/f/nbdoxgga'>
             <input 
               className='bg-transparent border-b py-3 outline-none w-full placeholder-text-white focus:border-accent transition-all'
               type='text'
               placeholder='Your name'
               value={name}
+              name='user'
               onChange={(e) => setName(e.target.value)}
             />
             <input 
@@ -35,13 +33,15 @@ const Contact = () => {
               type='text'
               placeholder='Your email'
               value={email}
+              name='email'
               onChange={(e) => setEmail(e.target.value)} // Set email state
             />
             <textarea 
               className='bg-transparent border-b py-12 outline-none w-full placeholder-text-white focus:border-accent transition-all resize-none mb-12'
               placeholder='Your message'
               value={message}
-              onChange={(e) => setMessage(e.target.value)} 
+              name='message'
+              onChange={(e) => setMessage(e.target.value)}
             />
             <button className='btn btn-lg' type="submit"> 
               Send message
